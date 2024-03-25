55b042299bd38e47731a4f401571cfd662390ae1
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-03-26 05:38:08 +0600

    conceptional session practice start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>      
+     
+      <p className="text-5xl">
+        Practice
+      </p>
+      
+    </>
+  )
+}
+
+export default App
